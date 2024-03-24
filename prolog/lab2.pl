30ee7804a6ed17bbf2c2f96c1ed9c7c4adfd5f9d
msjtw <fiedler.stanislaw@gmail.com>
2024-03-24 19:31:52 +0100

    prolog lab 2

--- prolog/lab2.pl
+++ prolog/lab2.pl
@@ -24,11 +24,15 @@ ruch(gołąb, bieganie).
 safe(state(X, W, X, C)).
 safe(state(X, X, G, X)).
 
-move(state(east, W, G, C), farmer_sam_west, state(west, W, G, C)).
-move(state(west, W, G, C), farmer_sam_east, state(east, W, G, C)).
-move(state(east, W, east, C), farmer_koza_west, state(west, W, west, C)).
-move(state(west, W, west, C), farmer_koza_east, state(east, W, east, C)).
-move(state(east, east, G, C), farmer_wilk_west, state(west, west, G, C)).
-move(state(west, west, G, C), farmer_wilk_east, state(east, east, G, C)).
-move(state(east, W, G, east), farmer_wilk_west, state(west, W, G, west)).
-move(state(west, W, G, west), farmer_wilk_east, state(east, W, G, west)).
+move(state(west, W, G, west), farmer_kapusta_east, state(east, W, G, east)) :- safe(state(east, W, G, east)).
+move(state(west, west, G, C), farmer_wilk_east, state(east, east, G, C)) :- safe(state(east, east, G, C)).
+move(state(west, W, west, C), farmer_koza_east, state(east, W, east, C)) :- safe(state(east, W, east, C)).
+move(state(west, W, G, C), farmer_sam_east, state(east, W, G, C)) :- safe(state(east, W, G, C)).
+
+move(state(east, W, G, C), farmer_sam_west, state(west, W, G, C)) :- safe(state(west, W, G, C)).
+move(state(east, W, east, C), farmer_koza_west, state(west, W, west, C)) :- safe(state(west, W, west, C)).
+move(state(east, east, G, C), farmer_wilk_west, state(west, west, G, C)) :- safe(state(west, west, G, C)).
+move(state(east, W, G, east), farmer_kapusta_west, state(west, W, G, west)) :- safe(state(west, W, G, west)).
+
+can_get(state(east,east,east,east)).
+can_get(Before) :- move(Before, Move, After), can_get(After).
