082335bbb8fca3b1ebfbeb2faa062a34dde77c7f
msjtw <fiedler.stanislaw@gmail.com>
2024-05-09 21:44:53 +0200

    gui

--- numerical_analysis/main.cpp
+++ numerical_analysis/main.cpp
@@ -16,6 +16,14 @@
 #include <GLES2/gl2.h>
 #endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include <iostream>
+#include <dlfcn.h>
+
+#include "interval.h"
+#include "newton_raphson.h"
+
+using std::string;
+const int TXTMX = 1024;
 
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 // To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
@@ -32,6 +40,47 @@
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+//load f, df and d2f functions
+template<typename T>
+int read_fuct(string path, T (*&f)(T), T (*&df)(T), T (*&d2f)(T)){
+    string suf = "f";
+    if(typeid(T) == typeid(Interval))
+        suf = "";
+    void* handle = dlopen(path.c_str(), RTLD_LAZY);
+    if (!handle) {
+        std::cerr << "Cannot load library: " << dlerror() << '\n';
+        return 1;
+    }
+
+    string name = "f"+suf;
+    f = (T (*)(T))dlsym(handle, name.c_str());
+    const char* dlsym_error = dlerror();
+    if (dlsym_error) {
+        std::cerr << "Cannot load symbol '" << suf << "f': " << dlsym_error << '\n';
+        dlclose(handle);
+        return 2;
+    }
+
+    name = "df"+suf;
+    df = (T (*)(T))dlsym(handle, name.c_str());
+    dlsym_error = dlerror();
+    if (dlsym_error) {
+        std::cerr << "Cannot load symbol '" << suf << "df': " << dlsym_error << '\n';
+        dlclose(handle);
+        return 3;
+    }
+
+    name = "f"+suf;
+    d2f = (T (*)(T))dlsym(handle, name.c_str());
+    dlsym_error = dlerror();
+    if (dlsym_error) {
+        std::cerr << "Cannot load symbol '" << suf << "d2f': " << dlsym_error << '\n';
+        dlclose(handle);
+        return 4;
+    }
+    return 0;
 }
 
 // Main code
@@ -103,14 +152,24 @@ int main(int, char**)
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\FonPts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
     //IM_ASSERT(font != nullptr);
 
     // Our state
     bool show_demo_window = true;
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
+    static char path[TXTMX] = "/home/msjtw/Documents/uni/numerical_analysis/ext/libext.so";
+    string libraryPath;
+    static int load_status = -1;
+    int calc_type = 0;
+    static char x[TXTMX], mit[TXTMX], eps[TXTMX], xupper[TXTMX], xlower[TXTMX];
+    Interval (*f)(Interval);
+    Interval (*df)(Interval);
+    Interval (*d2f)(Interval);
+    static _Float128 (*ff)(_Float128);
+    _Float128 (*dff)(_Float128);
+    _Float128 (*d2ff)(_Float128);
+    
     // Main loop
 #ifdef __EMSCRIPTEN__
     // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
@@ -139,36 +198,102 @@ int main(int, char**)
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
-            static float f = 0.0f;
-            static int counter = 0;
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
+            ImGui::Begin("Newton Raphson");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::SeparatorText("Library path");
+            ImGui::InputTextWithHint("##", "enter path to library containg functions", path, IM_ARRAYSIZE(path));
             ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            if (ImGui::Button("Load")){
+                libraryPath = path;
+                load_status = read_fuct<Interval>(libraryPath, f, df, d2f);
+                load_status = read_fuct<_Float128>(libraryPath, ff, dff, d2ff);
+            }
+            if (load_status >= 0)
+            {
+                string text;
+                if(load_status == 0){
+                    text = "Loaded";
+                    ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), text.c_str());
+                    // if (ImGui::TreeNode("Plotting")){
+                    //     static int display_count = 70;
+                    //     struct Funcs{
+                    //         static float func(void*, int i) { return ff(i); }
+                    //     };
+                    //     ImGui::PlotLines("Lines", Funcs::func, NULL, display_count, 0, NULL, -1.0f, 1.0f, ImVec2(0, 80));
+                    //     ImGui::TreePop();
+                    // }
+                }
+                else if(load_status == 1){
+                    text = "Cannot find: " + string(libraryPath);
+                    ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), text.c_str());
+                }
+                else{
+                    text = "Cannot load function symbol";
+                    ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), text.c_str());
+                }
+            }
+            if(load_status == 0){
+                ImGui::SeparatorText("Calculation Type");
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                ImGui::RadioButton("Float", &calc_type, 1); ImGui::SameLine();
+                ImGui::RadioButton("Point Interval", &calc_type, 2); ImGui::SameLine();
+                ImGui::RadioButton("Interval", &calc_type, 3);
+
+                if(calc_type == 1){
+                    ImGui::Text("Float");
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##x", "x", x, IM_ARRAYSIZE(x)); ImGui::SameLine();
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##mit", "max number of iterations", mit, IM_ARRAYSIZE(mit)); ImGui::SameLine();
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##eps", "desired epsilon", eps, IM_ARRAYSIZE(eps));
+                    int stat;
+                    string sx(x);
+                    string smit(mit);
+                    string seps(mit);
+                    static int clicked = 0;
+                    if (ImGui::Button("Calculate") and !sx.empty() and !smit.empty() and !seps.empty()){
+                        clicked++;
+                    }
+                    if (clicked & 1){
+                        _Float128 fatx;
+                        int it;
+                        _Float128 fx = strtof128(x, NULL);
+                        _Float128 feps = strtof128(eps, NULL);
+                        int imit = std::stoi(string(mit));
+                        stat = newton_raphson<_Float128>(fx, ff, dff, d2ff, imit, feps, fatx, it);
+                        string res = to_string(fx);
+                        ImGui::Text("resault");
+                        string stringstate;
+                        stringstate = std::to_string(stat);
+                        ImGui::Text(stringstate.c_str());
+                        ImGui::Text(res.c_str());
+                    }
+                }
+                else if(calc_type == 2){
+                    ImGui::Text("Point Interval");
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##xi", "x", x, IM_ARRAYSIZE(path)); ImGui::SameLine();
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##miti", "max number of iterations", mit, IM_ARRAYSIZE(path)); ImGui::SameLine();
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##epsi", "desired epsilon", eps, IM_ARRAYSIZE(path));
+                }
+                else if(calc_type == 3){
+                    ImGui::Text("Interval");
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##xii1", "x upper", xupper, IM_ARRAYSIZE(path)); ImGui::SameLine();
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##mitii", "max number of iterations", mit, IM_ARRAYSIZE(path)); ImGui::SameLine();
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##epsii", "desired epsilon", eps, IM_ARRAYSIZE(path));
+                    ImGui::SetNextItemWidth(ImGui::GetFontSize() * 20);
+                    ImGui::InputTextWithHint("##xii2", "x lower", xlower, IM_ARRAYSIZE(path));
+                }
+            }
             ImGui::End();
         }
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
 
         // Rendering
         ImGui::Render();
