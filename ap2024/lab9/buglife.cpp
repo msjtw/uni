48abce54ea5f154cddfc8caf6b610dc2aaef311d
msjtw <fiedler.stanislaw@gmail.com>
2024-05-22 19:29:22 +0200

    buglife ap lab9

--- ap2024/lab9/buglife.cpp
+++ ap2024/lab9/buglife.cpp
@@ -23,23 +23,27 @@ int main(){
         bool popsuted = false;
         int color[MX] {};
         int proc[MX] {};
-        queue<int>q; 
-        q.push(1);
-        color[1] = 1;
-        while(!q.empty() and !popsuted){
-            int p = q.front();
-            q.pop();
-            if(proc[p])
+        for(int i = 1; i < n; i++){
+            if(proc[i])
                 continue;
-            proc[p] = true;
+            queue<int>q; 
+            q.push(i);
+            color[i] = 1;
+            while(!q.empty() and !popsuted){
+                int p = q.front();
+                q.pop();
+                if(proc[p])
+                    continue;
+                proc[p] = true;
 
-            int c = -1*color[p];
-            for(int u : adj[p]){
-                if(color[u] == color[p]){
-                    popsuted = true;
+                int c = -1*color[p];
+                for(int u : adj[p]){
+                    if(color[u] == color[p]){
+                        popsuted = true;
+                    }
+                    color[u] = c;
+                    q.push(u);
                 }
-                color[u] = c;
-                q.push(u);
             }
         }
         cout << "Scenario #" << pp+1 << ":" << endl;
